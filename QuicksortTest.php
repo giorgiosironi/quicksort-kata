8201d9252270c43b8d146f5ea7d9a3136a75cf72
Giorgio Sironi <info@giorgiosironi.com>
2014-12-23 14:15:02 +0100

    Wrote first property: ordering of output

--- QuicksortTest.php
+++ QuicksortTest.php
@@ -1,11 +1,32 @@
 <?php
+use Eris\Generator;
+use Eris\TestTrait;
 
 class QuicksortTest extends \PHPUnit_Framework_TestCase
 {
-    use Eris\TestTrait;
+    use TestTrait;
 
     public function testSortsAnIntegerList()
     {
-        
-    }    
+        $this
+            ->forAll([
+                Generator\seq(Generator\int(-100, 100), Generator\nat(50))
+            ])
+            ->then(function($list) {
+                $result = quicksort($list);
+                $this->assertIsOrdered($result);
+            });
+    }
+
+    private function assertIsOrdered(array $list)
+    {
+        for ($i = 0; $i < count($list) - 1; $i++) {
+            $this->assertLessThanOrEqual($list[$i], $list[$i+1], var_export($list, true));
+        }
+    }
+}
+
+function quicksort(array $input)
+{
+    return [];
 }
